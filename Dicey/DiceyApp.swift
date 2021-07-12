b87c5e1524f4b4faf1feb45b6ff5e013c6832762
TeemoTrader <austinnorman1995@gmail.com>
2021-07-12 14:55:54 -0400

    Initial Commit

---
+++ Dicey/DiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceyApp.swift
+//  Dicey
+//
+//  Created by Teemo Norman on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
