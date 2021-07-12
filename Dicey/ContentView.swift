b7d9a941d492c2d52fd3be8cd3c9a4a397f0f7ad
TeemoTrader <austinnorman1995@gmail.com>
2021-07-12 15:57:03 -0400

    First Commit, Initial App

--- Dicey/ContentView.swift
+++ Dicey/ContentView.swift
@@ -8,8 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    @State var botRightDice = 1
+    @State var botLeftDice = 1
+    
+    
     var body: some View {
-        Text("Hello, world!")
+        let botScore = botRightDice + botLeftDice
+        let youScore = leftDiceNumber + rightDiceNumber
+        
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                VStack{
+                    if botScore > youScore {
+                        Text("BOT WINS!").bold()
+                    } else {
+                        Text("YOU WIN!").bold()
+                    }
+                    Text("You Score: \(leftDiceNumber + rightDiceNumber)").font(.system(size: 40))
+                    Text("Bot's Score: \(botLeftDice + botRightDice)").font(.system(size: 40))
+                }
+                .font(.system(size: 70))
+                .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                HStack{
+                    DiceView(n: botLeftDice)
+                    DiceView(n: botRightDice)
+                }
+                .frame(width: 300, height: 150)
+                .border(Color.black, width: 2)
+                HStack{
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+                }
+                .frame(width: 420, height: 250)
+                VStack{
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                    self.botLeftDice = Int.random(in: 1...6)
+                    self.botRightDice = Int.random(in: 1...6)
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.red)
+                        .padding()
+                        .frame(width: 300, height: 100, alignment: .center)
+                    
+                }
+                .background(Color.white)
+                }
+        }
+        }
+    }
+}
+struct DiceView: View {
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
             .padding()
     }
 }
@@ -19,3 +86,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
